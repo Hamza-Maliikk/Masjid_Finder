f4acc821f9612826a58f3255cc1bbf7d90abd123
Hamza Malik <hamzamalik123450@gmail.com>
2026-05-04 17:38:32 +0500

    update commit

--- my-app/src/lib/redis.ts
+++ my-app/src/lib/redis.ts
@@ -1,8 +1,9 @@
 import Redis from "ioredis"
 
-const redis = new Redis({
-  host: "localhost",
-  port: 6379,
+const redis = new Redis(process.env.REDIS_URL as string, {
+  tls: {
+    rejectUnauthorized: false,
+  },
 })
 
 redis.on("connect", () => console.log("✅ Redis Connected!"))
