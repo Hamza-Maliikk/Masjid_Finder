ad4ea31a4f576dbb689621645dc8d82c65b8c43b
Hamza Malik <hamzamalik123450@gmail.com>
2026-05-04 19:14:17 +0500

    update commit

--- my-app/src/lib/redis.ts
+++ my-app/src/lib/redis.ts
@@ -1,10 +1,6 @@
 import Redis from "ioredis"
 
-const redis = new Redis(process.env.REDIS_URL as string, {
-  tls: {
-    rejectUnauthorized: false,
-  },
-})
+const redis = new Redis(process.env.REDIS_URL as string)
 
 redis.on("connect", () => console.log("✅ Redis Connected!"))
 redis.on("error", (err) => console.error("❌ Redis Error:", err))
