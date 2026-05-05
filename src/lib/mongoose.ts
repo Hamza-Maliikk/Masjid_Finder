89a68ced8a00f6cfb4fb2ed6fed8c1f776a79cf1
Hamza Malik <hamzamalik123450@gmail.com>
2026-05-05 19:29:33 +0500

    update commit

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -6,7 +6,7 @@ const connectDB = async (): Promise<void> => {
   try{
     if (mongoose.connections[0].readyState) return
 
-  const uri = process.env.MONGODB_URI
+  const uri = "mongodb+srv://hamzascorpio03_db_user:hamzamalik1126@cluster0.lmluqbd.mongodb.net/?appName=Cluster0"
 
   if (!uri) {
     throw new Error('MONGODB_URI environment variable is not defined in .env')
